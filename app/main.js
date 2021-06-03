3ed60c284eda5da26348b1900953ce5f63d0379a
qwer11223 <1403112692@qq.com>
2021-06-04 00:26:48 +0800

    complete save_file

--- app/main.js
+++ app/main.js
@@ -3,6 +3,8 @@ const fs = require('fs')
 
 //多窗口集合
 const windows = new Set()   //es2015,集合，不允许重复项
+//打开的文件
+const openFiles = new Map() //窗口-文件监控器 键值对
 
 app.on('ready', () => {
     createWindow()
@@ -48,8 +50,28 @@ const createWindow = () => {
         newWindow.show()
     })
 
+    newWindow.on('close', e => {
+        if (newWindow.isDocumentEdited())
+            e.preventDefault()
+
+        const result = dialog.showMessageBoxSync(newWindow, {
+            type: 'warning',
+            title: 'Quit with Unsaved Changes?',
+            message: 'Your changes will be lost if you do not save.',
+            buttons: [
+                'Quit Anyway',
+                'Cancel'
+            ],
+            defaultId: 0,
+            cancelId: 1
+        })
+
+        if (result === 0) newWindow.destroy()
+    })
+
     newWindow.on('closed', () => { //窗口关闭后，设置渲染进程对象为null
         windows.delete(newWindow)
+        stopWatchingFile(newWindow) //窗口关闭后同时关闭与窗口关联的文件监控器
         newWindow = null
     })
 
@@ -73,11 +95,68 @@ const getFileFromUser = async (targetWindow) => {
 //打开文件
 const openFile = (targetWindow, file) => {
     const content = fs.readFileSync(file, { encoding: 'utf-8' }).toString()
+    app.addRecentDocument(file) //添加文件到系统最近打开列表中
     targetWindow.webContents.send('file-opened', file, content)
+    startWatchingFile(targetWindow,file)
+}
+
+//保存为html
+const saveHtml = (targetWindow, content) => {
+    const file = dialog.showSaveDialogSync(targetWindow, {
+        title: 'Save Html',
+        defaultPath: app.getPath('documents'),
+        filters: [
+            { name: 'HTML Files', extensions: ['html', 'htm'] }
+        ]
+    })
+
+    if (!file) return
+
+    fs.writeFileSync(file, content)
+}
+
+//保存文件
+const saveMarkdown = (targetWindow, file, content) => {
+    if (!file) {    //新文件，弹出对话框
+        file = dialog.showSaveDialog(targetWindow, {
+            title: 'Save Markdown',
+            defaultPath: app.getPath('documents'),
+            filters: [
+                { name: 'Markdown File', extensions: ['md', 'markdown'] }
+            ]
+        })
+    }
+
+    if (!file) return   //选中cancle按钮直接返回
+
+    fs.writeFileSync(file, content) //现有文件，直接保存
+    openFile(targetWindow, file)
+}
+
+//开启文件监控
+const startWatchingFile = (targetWindow, file) => {
+    stopWatchingFile(targetWindow)  //如果文件监控存在，取消监控
+
+    const watcher = fs.watch(file, e => {
+        if (e === 'change') {
+            const content = fs.readFileSync(file).toString()
+            targetWindow.webContents.send('file-opened', file, content)
+        }
+    })
+
+    openFiles.set(targetWindow, watcher)
+}
+
+//停止文件监控
+const stopWatchingFile = targetWindow => {
+    if (openFiles.has(targetWindow)) {
+        openFiles.get(targetWindow).stop()
+        openFiles.delete(targetWindow)
+    }
 }
 
 
 /////// export modules ///////
 module.exports = {
-    getFileFromUser, createWindow
+    getFileFromUser, createWindow, saveHtml, saveMarkdown, openFile
 }
--- app/renderer.js
+++ app/renderer.js
@@ -1,6 +1,11 @@
 const marked = require('marked')
 const { remote, ipcRenderer } = require('electron')
 const mainProcess = remote.require('./main.js')
+const path = require('path')
+
+const currentWindow = remote.getCurrentWindow() //当前窗口
+let filePath = null //文件路径
+let originalContent = ''    //文件原内容
 
 //view
 const markdownView = document.querySelector('#markdown')
@@ -15,27 +20,106 @@ const saveHtmlBtn = document.querySelector('#save-html')
 const showFileBtn = document.querySelector('#show-file')
 const openInDefaultBtn = document.querySelector('#open-in-default')
 
-//convert
+
+//drag event
+document.addEventListener('dragstart', e => e.preventDefault())
+document.addEventListener('dragover', e => e.preventDefault())
+document.addEventListener('dragleave', e => e.preventDefault())
+document.addEventListener('drop', e => e.preventDefault())
+
+///// function //////
 const renderMarkdownToHtml = markdown => {
     // console.log(marked(markdown, { sanitize: true }))
     htmlView.innerHTML = marked(markdown, { sanitize: true })
 }
 
+const updateUserInterface = (isEdit) => {     //修改标题
+    let title = 'MarkEdit'
+
+    if (filePath)
+        title = `${path.basename(filePath)} - ${title}`
+    if (isEdit)
+        title = `${title} (Edited)`
+
+    currentWindow.setTitle(title)
+    currentWindow.setDocumentEdited(isEdit) //macos有效，影响窗口样式
+
+    //启用按钮
+    saveMarkdownBtn.disabled = !isEdit
+    revertBtn.disabled = !isEdit
+}
+
+//  get drag/drop file
+const getDraggedFile = e => e.dataTransfer.items[0]
+const getDroppedFile = e => e.dataTransfer.files[0]
+
+const fileTypeIsSupported = file => {
+    return ['text/plain', 'text/markdown'].includes(file.type)
+}
+
 
 ////// events ////////
 markdownView.addEventListener('keyup', e => {
+    const currentContent = e.target.value
+
     renderMarkdownToHtml(e.target.value)
+    updateUserInterface(currentContent != originalContent)
+
 })
 
 openFileBtn.addEventListener('click', () => {
-    mainProcess.getFileFromUser(remote.getCurrentWindow())   //调用主进程方法
+    mainProcess.getFileFromUser(currentWindow)   //调用主进程方法
 })
 
 ipcRenderer.on('file-opened', (event, file, content) => {
-    markdownView.value = content
-    renderMarkdownToHtml(content)
+    filePath = file //将当前所打开的文件路径保存到全局变量
+    originalContent = content   //将原始内容保存到全局变量，检测文件是否修改
+
+    markdownView.value = content    //覆盖textarea区域内容
+    renderMarkdownToHtml(content)   //渲染到markdown区域
+
+    updateUserInterface()
 })
 
-newFileBtn.addEventListener('click',()=>{
+newFileBtn.addEventListener('click', () => {
     mainProcess.createWindow()
+})
+
+saveHtmlBtn.addEventListener('click', () => {
+    mainProcess.saveHtml(currentWindow, htmlView.innerHTML)
+})
+
+saveMarkdownBtn.addEventListener('click', () => {
+    mainProcess.saveMarkdown(currentWindow, filePath, markdownView.value)
+})
+
+revertBtn.addEventListener('click', () => {
+    markdownView.value = originalContent
+    renderMarkdownToHtml(originalContent)
+})
+
+markdownView.addEventListener('dragover', e => {
+    const file = getDraggedFile(e)
+
+    if (fileTypeIsSupported(file))
+        markdownView.classList.add('drag-over')
+    else
+        markdownView.classList.add('drag-error')
+})
+
+markdownView.addEventListener('dragleave', e => {
+    markdownView.classList.remove('drag-over')
+    markdownView.classList.remove('drag-error')
+})
+
+markdownView.addEventListener('drop', e => {
+    const file = getDroppedFile(e)
+
+    if (fileTypeIsSupported(file))
+        mainProcess.openFile(currentWindow, file.path)
+    else
+        alert('That file type is not supported')
+
+    markdownView.classList.remove('drag-over')
+    markdownView.classList.remove('drag-error')
 })
